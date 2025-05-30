7e4c4cd06f602d9c89d2b71339acd217a444236b
HiTunn <tunglt.2401@gmail.com>
2025-05-30 23:13:01 +0700

    Feat: Add Settings class for application configuration and environment management

---
+++ config/settings.py
@@ -0,0 +1,22 @@
+import os
+from functools import lru_cache
+from pydantic_settings  import BaseSettings
+
+class Settings(BaseSettings):
+    """Application settings"""
+    
+    cohere_api_key: str
+    chunk_size: int = 1000
+    chunk_overlap: int = 200
+    retrieval_k: int = 3
+    max_tokens: int = 1000
+    temperature: float = 0.1
+    llm: str = "command-r-plus"
+    embedding_model_name: str = "embed-english-v3.0"
+    
+    class Config:
+        env_file = ".env"
+
+@lru_cache()
+def get_settings():
+    return Settings()
